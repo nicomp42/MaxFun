2d13a08418271609ba08dcbc1c1224459e1a411f
nicomp42 <nicholdw@ucmail.uc.edu>
2023-11-03 18:35:25 -0400

    First Commit

---
+++ MaxFun/functionPackage/functions.py
@@ -0,0 +1,10 @@
+# functions.py
+
+def findMax(myList):
+    '''
+    @param myList: The list to be processed
+    @return: the max value in myList or 7 if there is at least one
+    7 in myList
+    '''
+    
+    return None
---
+++ MaxFun/mainPackage/main.py
@@ -0,0 +1,12 @@
+# main.py
+
+from functionPackage.functions import *
+
+if __name__ == "__main__":
+    data = [15,42,1,-9,100,12,6]
+    
+    result = findMax(data)
+    
+    print("result:", result)
+
+    print("result:", [1,2,3,7,6,7,1])
